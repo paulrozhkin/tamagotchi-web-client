1557c16021c6052346d3000ff41d96e30f9f3625
Paul Rozhkin <blackiiifox@gmail.com>
2020-05-03 16:46:42 +0300

    Almost finished index markup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TamagotchiClient';
+export class AppComponent implements OnInit{
+  title = 'tamagotchi-web-client';
+
+  ngOnInit(): void {
+    AOS.init();
+  }
+
+
 }
