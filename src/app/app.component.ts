37a96f57fe8ae5f1b8d373c192cb195a80a57e70
Paul Rozhkin <blackiiifox@gmail.com>
2020-05-03 19:27:35 +0300

    Index markup completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import * as AOS from 'aos';
+import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,22 @@ import * as AOS from 'aos';
 export class AppComponent implements OnInit{
   title = 'tamagotchi-web-client';
 
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+
   ngOnInit(): void {
     AOS.init();
   }
 
 
+  open(content) {
+    this.modalService.open(content);
+  }
+
+  downloadApkUrl() {
+    return 'http://316825-blackiiifox.tmweb.ru:3000/api/files/0';
+  }
 }
